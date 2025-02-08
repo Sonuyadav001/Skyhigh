634622886c7aa2f9a3f569e9f7ab85a28244e4b4
Sonuyadav001 <75711473+Sonuyadav001@users.noreply.github.com>
2025-02-08 10:25:24 +0700

    Delete brand directory

--- brand/urls.py
+++
@@ -1,9 +0,0 @@
-# C:\Users\Admin\Desktop\skyhigh\brand\urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('area/', views.brand_area, name='brand_area'),  # For homepage
-    path('list/', views.brand_list, name='brand_list'),  # For dedicated brand list page
-    path('<int:brand_id>/', views.brand_detail, name='brand_detail'),  # For brand detail page
-]
